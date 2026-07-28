f24bd043913f45d9081fe389ccba95f19128ee0b
root <root@WIN-9GHPCVAF27I.localdomain>
2026-07-28 21:30:53 +0500

    fdsfds

--- drivers/devfreq/helio-dvfsrc-opp.c
+++ drivers/devfreq/helio-dvfsrc-opp.c
@@ -11,7 +11,7 @@
  * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
  */
 
-#include <helio-dvfsrc.h>
+#include "helio-dvfsrc.h"
 #include <helio-dvfsrc-opp.h>
 
 static struct opp_profile opp_table[VCORE_DVFS_OPP_NUM];
